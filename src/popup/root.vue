c38f02fe1727bf7c00d8a45ca3b98976b5db9e26
Ludwig Stockbauer-Muhr <ludwig.stockbauer-muhr@msg.group>
2018-08-21 20:28:12 +0200

    refactor(popup): introduce tab pane to show history/options

--- src/popup/root.vue
+++ src/popup/root.vue
@@ -4,41 +4,46 @@
             <img class="popup-icon" src="../../static/icons/Icon128.png" alt="image">
             <h2>MS Office Link</h2>
         </div>
-        <h2>Link History</h2>
-        <el-table :data="tableData"
-                  border
-                  height="350"
-                  style="width: 100%">
-            <el-table-column label="Type"
-                             width="60">
-                <template slot-scope="scope">
-                    <img v-if="scope.row.type === 'word'" src="../../static/icons/word-app.svg" width="20px" alt="word">
-                    <img v-else-if="scope.row.type === 'excel'" src="../../static/icons/excel-app.svg" width="20px"
-                         alt="word">
-                    <img v-else src="../../static/icons/powerpoint-app.svg" width="20px" alt="word">
-                </template>
-            </el-table-column>
-            <el-table-column prop="file"
-                             label="Name">
-            </el-table-column>
-            <el-table-column label="Action" width="70">
-                <template slot-scope="scope">
-                    <el-button round icon="el-icon-more" circle></el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-        <hr>
-        <el-button round>Show full history</el-button>
-        <el-button round>Options</el-button>
-        <!--<form-entry class="flex flex&#45;&#45;row flex&#45;&#45;align-center" label="Activate Options">-->
-            <!--<el-switch v-model="form.delivery"></el-switch>-->
-        <!--</form-entry>-->
-        <!--<form-entry class="flex flex&#45;&#45;row flex&#45;&#45;align-center" label="Default Action">-->
-            <!--<el-select v-model="form.region" placeholder="please select your zone">-->
-                <!--<el-option label="Zone one" value="shanghai"></el-option>-->
-                <!--<el-option label="Zone two" value="beijing"></el-option>-->
-            <!--</el-select>-->
-        <!--</form-entry>-->
+        <el-tabs class="popup-tabs">
+            <el-tab-pane label="Link History" class="popup-tabs__history flex flex--column">
+                <el-table :data="tableData"
+                          border
+                          height="350"
+                          style="width: 100%">
+                    <el-table-column label="Type"
+                                     width="60">
+                        <template slot-scope="scope">
+                            <img v-if="scope.row.type === 'word'" src="../../static/icons/word-app.svg" width="20px"
+                                 alt="word">
+                            <img v-else-if="scope.row.type === 'excel'" src="../../static/icons/excel-app.svg"
+                                 width="20px"
+                                 alt="word">
+                            <img v-else src="../../static/icons/powerpoint-app.svg" width="20px" alt="word">
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="file"
+                                     label="Name">
+                    </el-table-column>
+                    <el-table-column label="Action" width="70">
+                        <template slot-scope="scope">
+                            <el-button round icon="el-icon-more" circle></el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+                <el-button class="show-history-btn">Show full history</el-button>
+            </el-tab-pane>
+            <el-tab-pane label="Options">
+                <form-entry class="flex flex--row flex--align-center" label="Activate Options">
+                    <el-switch v-model="form.delivery"></el-switch>
+                </form-entry>
+                <form-entry class="flex flex--row flex--align-center" label="Default Action">
+                    <el-select v-model="form.region" placeholder="please select your zone">
+                        <el-option label="Zone one" value="shanghai"></el-option>
+                        <el-option label="Zone two" value="beijing"></el-option>
+                    </el-select>
+                </form-entry>
+            </el-tab-pane>
+        </el-tabs>
     </div>
 </template>
 
@@ -85,15 +90,21 @@ hr {
     padding: 5px 0;
   }
 
+  .el-tabs__item {
+    font-size: 15px;
+  }
+
   width: 500px;
 
   .form-entry__label {
     width: 120px;
+    font-size: 14px;
   }
 }
 
 .popup-icon {
   width: 50px;
+
   margin-right: 15px;
 }
 
@@ -105,5 +116,15 @@ hr {
   box-shadow: 1px 1px 10px inset #0064a7;
   color: white;
   margin: 0 -15px 0 0;
+}
+
+.popup-tabs {
+  padding: 5px;
+}
+
+.popup-tabs__history {
+  .show-history-btn {
+    margin: 5px 0 0 0;
+  }
 }
 </style>
