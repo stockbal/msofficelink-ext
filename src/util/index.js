ae8829ab885e7f7c75c0d4a042017f22260298d1
stockbal <ludwig.stockbauer-muhr@msg.group>
2018-11-29 08:18:34 +0100

    fix: minor semantic mistake -> word/excel endings were switched

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,7 @@
 import { ExtStorage } from '../ext/storage';
 
-const excelFileEndings = ['doc', 'docx', 'docm'];
-const wordFileEndings = ['xls', 'xlsx', 'xlsm', 'csv', 'xlsb'];
+const wordFileEndings = ['doc', 'docx', 'docm'];
+const excelFileEndings = ['xls', 'xlsx', 'xlsm', 'csv', 'xlsb'];
 const powerPointFileEndings = ['ppt', 'pptx', 'pptm'];
 
 /**
